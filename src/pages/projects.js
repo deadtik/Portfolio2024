68aefde1c607c359c5c9784b62a7c55cf5fd27a9
techateshq <techateshq@gmail.com>
2024-08-07 09:49:19 +0000

    Projects uploading, component ready!

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,6 +2,32 @@ import React from 'react';
 import Head from 'next/head';
 import Layout from '@/components/Layout';
 import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
+import Image from 'next/image';
+import { GithubIcon } from '@/components/Icons';
+
+const FeaturedProjects= ({type, title, summary, img, link, github}) => {
+  return(
+    <article>
+      <Link href={link} target='_blank'>
+      <Image src={img} alt={title} className='w-full h-auto'/>
+      </Link>
+
+         <div>
+           <span>{type}</span>
+              <Link href={link} target='_blank'>
+                <h2>{title}</h2>
+              </Link>
+              <p>{summary}</p>
+              <div>
+                <Link href={github} target='_blank'> <GithubIcon /> </Link>
+                <Link href={link} target='_blank'> Visit the Project </Link>              
+              </div>
+</div>
+</article>
+
+  )
+}
 
 const Projects = () => {
 return (
@@ -11,14 +37,16 @@ return (
         <meta name='description' content='any description' />
       </Head>
       <main className='flex w-full flex-col items-center justify-center mb-16'>
-          <Layout>
-              <AnimatedText text='Featured Development Projects'/>
+          <Layout className='pt-16'>
+              <AnimatedText text='Featured Projects!'/>
 
             <div className='grid grid-col-12 gap-24'>
                 <div className='col-span-12'>
-                    Project 0
+                    <FeaturedProjects />
                 </div>
                 <div className='col-span-6'>Project 1</div>
+                <div className='col-span-6'> Project 2 </div>
+                <div className='col-span-6'> Project 2 </div>
                 <div className='col-span-6'> Project 2 </div>
             </div>
 
